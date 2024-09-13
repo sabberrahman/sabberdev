ceaf1d2e76369a46d72cf003542e4c8579702dd6
sabberrahman <sabbersr1@gmail.com>
2024-09-14 01:11:56 +0600

    favicon bug

--- components/shared/Infomation.jsx
+++ components/shared/Infomation.jsx
@@ -11,7 +11,19 @@ function Infomation() {
            Hi, I'm Shohanur rahman sabbir. A self-taught, frontend-focused full-stack developer based in Dhaka, Bangladesh. Crafting Modern Web Application with my রং চা and Code 
           </p>
 
-          <p className='text-sm -mt-2'>Currently im exploring <span className='text-green-600 font-bold italic underline underline-offset-1'>Next.js</span>, <span className='text-green-600 font-bold italic underline underline-offset-1'>Nest.js</span> and <span className='text-green-600 font-semibold italic '>SQL</span> and some other interesting tools</p>
+          <p className='text-sm -mt-2'>Currently, I'm exploring {""}
+          <span className='relative inline-block text-white'>
+        <span className='absolute inset-0 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded p-1'></span>
+        <span className='relative z-10 text-green-400 font-bold italic p-2 '>Next.js</span>
+      </span> {""}
+      <span className='relative inline-block text-white'>
+        <span className='absolute inset-0 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded p-1'></span>
+        <span className='relative z-10 text-green-400 font-bold italic p-2'>GraphQL</span>
+      </span>  and
+      <span className='relative inline-block text-white'>
+        <span className='absolute inset-0 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded p-1'></span>
+        <span className='relative z-10 text-green-400 font-semibold italic p-2'>Jest</span>
+      </span> with some other interesting tools</p>
           <p className='text-xs italic text-muted-foreground'>Ping me for Custom Projects</p>
           
           <p className="text-green-500 flex gap-2 opacity-90 text-sm"><LucideDot className='animate-ping'/> Available for new opportunities</p>
